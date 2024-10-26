4aba329c8521e5987f2a45f6195d7ff5bd4fd311
ShazibHassan348 <shazibhassan49@gmail.com>
2024-10-26 18:29:55 +0600

    Commit

---
+++ src/test/java/test/MainTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MainTest {
+
+    @Test
+    public void test(){
+        int result = 2 + 3;
+        Assert.assertEquals(result,5,"2+3 should equal 5");
+    }
+
+
+
+}
