ca8daaa63b7257668b1850564c9d9e89f80f4aae
ShazibHassan348 <shazibhassan49@gmail.com>
2025-01-24 11:54:47 +0600

    New Commit

--- src/test/java/test/MainTest.java
+++ src/test/java/test/MainTest.java
@@ -1,14 +1,21 @@
 package test;
 
+import aquality.selenium.browser.AqualityServices;
+import aquality.selenium.browser.Browser;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MainTest{
 
+    Browser browser = AqualityServices.getBrowser();
+
     @Test
     public void test(){
+        browser.goTo("https://dev.app.nuport.io/login");
         int result = 2 + 3;
         Assert.assertEquals(result,5,"2+3 should equal 5");
+
+        browser.quit();
         /// Maintest
     }
 
